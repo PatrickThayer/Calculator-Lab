34416a134d9ac9a370bed1451e5b90f9f63f527a
Patar595 <pthayer595@yahoo.com>
2022-07-13 12:18:43 -0400

    Set up button factory

---
+++ Calculator Lab/ButtonFactory.cpp
@@ -0,0 +1,112 @@
+#include "ButtonFactory.h"
+
+
+wxButton* ButtonFactory::GetButtonBin()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonHex()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonDec()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton0()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton1()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton2()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton3()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton4()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton5()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton6()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton7()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton8()
+{
+
+}
+
+wxButton* ButtonFactory::GetButton9()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonClear()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonEquals()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonPlus()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonMinus()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonTimes()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonDivide()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonModulo()
+{
+
+}
+
+wxButton* ButtonFactory::GetButtonPlusOrMinus()
+{
+
+}
+
+wxTextCtrl* ButtonFactory::GetTextBox()
+{
+
+}
---
+++ Calculator Lab/ButtonFactory.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "wx/wx.h"
+
+class ButtonFactory
+{
+
+private:
+	wxButton* GetButtonBin();
+	wxButton* GetButtonHex();
+	wxButton* GetButtonDec();
+	wxButton* GetButton0();
+	wxButton* GetButton1();
+	wxButton* GetButton2();
+	wxButton* GetButton3();
+	wxButton* GetButton4();
+	wxButton* GetButton5();
+	wxButton* GetButton6();
+	wxButton* GetButton7();
+	wxButton* GetButton8();
+	wxButton* GetButton9();
+	wxButton* GetButtonClear();
+	wxButton* GetButtonEquals();
+	wxButton* GetButtonPlus();
+	wxButton* GetButtonMinus();
+	wxButton* GetButtonTimes();
+	wxButton* GetButtonDivide();
+	wxButton* GetButtonModulo();
+	wxButton* GetButtonPlusOrMinus();
+
+	wxTextCtrl* GetTextBox();
+};
